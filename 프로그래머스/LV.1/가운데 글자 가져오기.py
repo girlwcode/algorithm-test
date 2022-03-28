ce53670cf58854971ca26dd0983387ca4a1ac99f
yelynAhn <yelynahn@gmail.com>
2022-03-29 00:11:59 +0900

    [22.03.29] DAY14

---
+++ 프로그래머스/LV.1/가운데 글자 가져오기.py
@@ -0,0 +1,16 @@
+"""
+단어 s의 가운데 글자를 반환하는 함수, solution을 만들어 보세요. 단어의 길이가 짝수라면 가운데 두글자를 반환하면 됩니다.
+
+재한사항
+s는 길이가 1 이상, 100이하인 스트링입니다.
+"""
+
+def solution(s):
+    answer = ''
+    num = len(s)//2
+    if (len(s) % 2 == 0):
+        answer = s[num -1: num+1]
+    else: 
+        answer = s[num]
+    return answer
+
